c717489ef7f6ddcceba4a45c75db7251b72b9ce9
Pat Smith <patsmith@gmail.com>
2021-03-19 20:58:19 -0400

    Completed Weekly Challenge01

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -4,6 +4,10 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
+        public ChallengesSet01()
+        {
+        }
+
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
@@ -18,32 +22,33 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int min = Math.Min(number1, number2);
+            return min;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2; 
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return (nameOfPerson == null || nameOfPerson == String.Empty)? $"Hello!" : $"Hello, {nameOfPerson}!";
         }
-
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
+        
+            public string GetHey()
+            {
+                return $"HEY!";
+            }
     }
 }
